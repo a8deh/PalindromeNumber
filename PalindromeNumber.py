89c4c3bb89cd7232d8fbf347692890cbd79084ea
Abdelhadi HADDADI <126632654+abdelhadi-haddadi@users.noreply.github.com>
2024-01-18 22:56:46 +0100

    Add files via upload

---
+++ PalindromeNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+  def isPalindrome(self, x: int) -> bool:
+      x = str(x)
+      n = x[::-1]
+      if x == n:
+        return True
+      else:
+        return False
+print(Solution().isPalindrome(133))
